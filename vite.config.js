8b08fe840cb3479a7bdf2ba1007e5059d5118374
Alexsandra-m-silva <142628895+Alexsandra-m-silva@users.noreply.github.com>
2026-04-06 18:41:12 +0200

    build hobbies section

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: './',
   build: {
-    outDir: 'docs', // build output will go into /docs 
+    outDir: 'docs', // build output will go into /docs
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        hobbies: './public/Pages/hobbies.html'
+      }
+    }
   },
 });
